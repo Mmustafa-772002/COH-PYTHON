896097ce982fc64057bfa621e08df8614dbb082a
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-02-23 16:13:30 +0530

    uoto sets syllabus added

--- 6.py
+++ 6.py
@@ -1,30 +1,30 @@
 # dictionary and set - the dictionary is a data type in python that is used to store key-value pairs. A set is a collection of unique elements.
-# clear()	 
-# copy()	
-# fromkeys()	
-# get()	
-# items()	
-# keys()	
-# pop()	
-# popitem()	
-# setdefault()	
-# update()	
-# values()	
+# clear()
+# copy()
+# fromkeys()
+# get()
+# items()
+# keys()
+# pop()
+# popitem()
+# setdefault()
+# update()
+# values()
 
 # clear() - 	Removes all the elements from the dictionary
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example.clear()
 print(example)
 
 # copy() - 	Returns a copy of the dictionary
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.copy()
 print(example1)
 
 # fromkeys() - 	Returns a dictionary with the specified keys and value
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.fromkeys(["brand", "model"], "default_value")
 print(example1)
@@ -32,7 +32,7 @@ print(example1)
 
 # get() - 	Returns the value of the specified key
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.get("brand")
 print(example1)
@@ -40,7 +40,7 @@ print(example1)
 
 # items() - 	Returns a list containing a tuple for each key value pair
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.items()
 print(example1)
@@ -48,7 +48,7 @@ print(example1)
 
 # keys() - 	Returns a list containing the dictionary's keys
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.keys()
 print(example1)
@@ -56,7 +56,7 @@ print(example1)
 
 # pop() - 	Removes the element with the specified key
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example.pop("model")
 print(example)
@@ -64,7 +64,7 @@ print(example)
 
 # popitem() - 	Removes the last inserted key-value pair
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example.popitem()
 print(example)
@@ -72,21 +72,21 @@ print(example)
 
 # setdefault() - 	Returns the value of the specified key. If the key does not exist: insert the key, with the specified value
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.setdefault("model", "Bronco")
 print(example1)
 
 # update() - 	Updates the dictionary with the specified key-value pairs
 
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example.update({"color": "red"})
 print(example)
 
 
 # values() - 	Returns a list of all the values in the dictionary
-example = { "brand": "Ford", "model": "Mustang", "year": 1964 }
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 example1 = example.keys()
 print(example1)
@@ -94,11 +94,7 @@ example2 = example.values()
 print(example2)
 
 
-example = {
-    "brand": "Ford",
-    "model": "Mustang",
-    "year": 1964
-}
+example = {"brand": "Ford", "model": "Mustang", "year": 1964}
 print(example)
 print(example["brand"])
 print(example["model"])
@@ -135,24 +131,23 @@ print(example)
 # set -  it is the collection of unique elements and it is unordered and unindexed and it is mutable and it is written with curly brackets. and the set is also used to perform the mathematical operations like union, intersection, difference, symmetric difference, etc.
 # the set methods
 
-# add()	
+# add()
 # clear()
 # copy()
-# difference()	
+# difference()
 # difference_update()
-# discard()	
-# intersection()	
-# intersection_update()	
-# isdisjoint()	
-# issubset()	
-# issuperset()	
-# pop()	
-# remove()	
-# symmetric_difference()	
-# symmetric_difference_update()	
-# union()	
-# update()	
-
+# discard()
+# intersection()
+# intersection_update()
+# isdisjoint()
+# issubset()
+# issuperset()
+# pop()
+# remove()
+# symmetric_difference()
+# symmetric_difference_update()
+# union()
+# update()
 
 
 # add()	-  Adds an element to the set
@@ -201,7 +196,7 @@ example = {"apple", "banana", "cherry"}
 example1 = {"google", "microsoft", "apple"}
 print(example.isdisjoint(example1))
 # issubset()	-  Returns whether another set contains this set or not
-example = {"apple", "banana", "cherry"} 
+example = {"apple", "banana", "cherry"}
 example1 = {"google", "microsoft", "apple"}
 print(example.issubset(example1))
 # issuperset()	-  Returns whether this set contains another set or not
@@ -218,7 +213,28 @@ example = {"apple", "banana", "cherry"}
 print(example)
 example.remove("apple")
 # symmetric_difference()	-  Returns a set with the symmetric differences of two sets
+example = {"apple", "banana", "cherry"}
+example1 = {"google", "microsoft", "apple"}
+print(example.symmetric_difference(example1))
+
+
 # symmetric_difference_update()	-  inserts the symmetric differences from this set and another
+example = {"apple", "banana", "cherry"}
+example1 = {"google", "microsoft", "apple"}
+print(example.symmetric_difference_update(example1))
 # union()	-  Return a set containing the union of sets
+
+example = {"apple", "banana", "cherry"}
+example1 = {"google", "microsoft", "apple"}
+print(example.union(example1))
 # update()	-  Update the set with another set, or any other iterable
+
+example = {"apple", "banana", "cherry"}
+example1 = {"google", "microsoft", "apple"}
+example.update(example1)  # TODO complete next  
+print(example)
+
+
+
+
 
