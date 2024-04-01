3761754dbbb0a1b7f37c83895d3603240e47d2f5
mustafa <mustafa.rpzyu20@sinhgad.edu>
2024-04-01 09:39:46 +0530

    added upto new files

--- 13.py
+++ 13.py
@@ -91,6 +91,7 @@ print(p.company)
 # multilevel inheritance : when a class inherits a class and that class inherits another class then it is called multilevel inheritance (as shown below)
 # 3. multilevel inheritance - one parent and one child and that child is parent of another child class
 
+
 class person:
     country = "India"
 
@@ -134,12 +135,13 @@ p.takeBreak()
 
 class person:
     country = "India"
+
     def __init__(self):
         print("in init of person")
-    
 
     def takeBreak(self):
         print("I am taking break")
+
 
 class employee(person):
     company = "honda"
@@ -162,20 +164,465 @@ e.takeBreak()
 # class method : A class method is a method that is bound to the class rather than the object of the class. It is a method that is called on the class rather than the object of the class.
 # class method is used when we want to access the class variable in the method
 
+
 class employee:
     company = "google"
     salary = 100
-    location= "delhi"
-    
-    def changeSalary(self,sal):
+    location = "delhi"
+
+    def changeSalary(self, sal):
         self.salary = sal
-    
+
+
 e = employee()
 print(e.salary)
 print(e.location)
 e.changeSalary(200)
 print(e.salary)
 
-
 # hierarchical inheritance : when a class is inherited by multiple classes then it is called hierarchical inheritance
+
+# TODO : add example of hierarchical inheritance
 # hybrid inheritance : when a class is inherited by multiple classes and those classes are inherited by other classes then it is called hybrid inheritance
+
+# TODO : add example of hybrid inheritance
+
+# method overloading : Method overloading is a feature that allows a class to have more than one method having the same name, if their argument lists are different. It is similar to constructor overloading in Java.
+
+
+class Number:
+    def __init__(self, num):
+        self.num = num
+
+    def __add__(self, num2):
+        print("let's add")
+        return self.num + num2.num
+
+    def __mul__(self, num2):
+        print("let's multiply")
+        return self.num * num2.num
+
+
+n1 = Number(2)
+n2 = Number(8)
+sum = n1 + n2
+mul = n1 * n2
+print(sum)
+print(mul)
+# operator in the python are overloaded by using the dunder methods
+
+# operator can be overloaded using following dunder methods
+# p1 + p2  => p1.__add__(p2)
+# p1 - p2  => p1.__sub__(p2)
+# p1 * p2  => p1.__mul__(p2)
+# p1 / p2  => p1.__div__(p2)
+# p1 // p2  => p1.__floordiv__(p2)
+# p1 % p2  => p1.__mod__(p2)
+# p1 ** p2  => p1.__pow__(p2)
+# p1 += p2  => p1.__iadd__(p2)
+# p1 -= p2  => p1.__isub__(p2)
+# p1 *= p2  => p1.__imul__(p2)
+# p1 /= p2  => p1.__idiv__(p2)
+# p1 //= p2  => p1.__ifloordiv__(p2)
+# p1 %= p2  => p1.__imod__(p2)
+# p1 **= p2  => p1.__ipow__(p2)
+# p1 == p2  => p1.__eq__(p2)
+
+
+# other magic methods
+#   __str__() : returns string
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __str__(self):
+        return f"The name is {self.name} and salary is {self.salary}"
+
+
+e = employee()
+e.name = "Rohan"
+print(e)
+
+# __rep__() : returns string
+# example :
+
+
+class employee:
+    company = "google"
+    salary = 100
+
+    def __repr__(self):
+        return f"The name is {self.name} and salary is {self.salary}"
+
+
+e = employee()
+e.name = "Rohan"
+print(e)
+
+# __len__() : returns length of the object
+# example :
+
+
+class employee:
+    company = "google"
+    salary = 100
+
+    def __len__(self):
+        return len(self.name)
+
+
+e = employee()
+e.name = "Rohan"
+print(len(e))
+
+# __getitem__() : returns the item of the object
+# example :
+
+
+class employee:
+    company = "google"
+    salary = 100
+
+    def __getitem__(self, index):
+        return self.name[index]
+
+
+e = employee()
+e.name = "Rohan"
+print(e[1])
+
+
+# __setitem__() : sets the item of the object
+# # example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __setitem__(self, index, value):
+        self.name = list(self.name)
+        self.name[index] = value
+        self.name = "".join(self.name)
+
+
+e = employee()
+e.name = "Rohan"
+e[1] = "s"
+print(e.name)
+
+# TODO : fix the above code to work correctly  -- done
+
+
+# __delitem__() : deletes the item of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __delitem__(self, index):
+        self.name = self.name[:index] + self.name[index + 1 :]
+
+
+e = employee()
+e.name = "Rohan"
+del e[1]
+print(e.name)
+
+
+# __contains__() : returns true if the item is present in the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __contains__(self, value):
+        return value in self.name
+
+
+e = employee()
+e.name = "Rohan"
+print("R" in e)
+
+
+# __call__() : calls the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __call__(self):
+        return "This is a callable object"
+
+
+e = employee()
+print(e())
+
+
+# __iter__() : returns the iterator object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __iter__(self):
+        self.x = 0
+        return self
+
+    def __next__(self):
+        if self.x < 5:
+            self.x += 1
+            return self.x
+        else:
+            raise StopIteration
+
+
+e = employee()
+for i in e:
+    print(i)
+
+
+# __next__() : returns the next item in the iterator
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __iter__(self):
+        self.x = 0
+        return self
+
+    def __next__(self):
+        if self.x < 5:
+            self.x += 1
+            return self.x
+        else:
+            raise StopIteration
+
+
+e = employee()
+for i in e:
+    print(i)
+
+
+# __add__() : returns the addition of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __add__(self, other):
+        return self.salary + other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 + e2)
+
+
+# __sub__() : returns the subtraction of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __sub__(self, other):
+        return self.salary - other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 - e2)
+
+
+# __mul__() : returns the multiplication of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __mul__(self, other):
+        return self.salary * other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 * e2)
+
+
+# __truediv__() : returns the division of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __truediv__(self, other):
+        return self.salary / other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 / e2)
+
+
+# __floordiv__() : returns the floor division of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __floordiv__(self, other):
+        return self.salary // other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 // e2)
+
+
+# __mod__() : returns the modulus of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __mod__(self, other):
+        return self.salary % other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1 % e2)
+
+
+# __pow__() : returns the power of the object
+# example :
+class employee:
+    company = "google"
+    salary = 100
+
+    def __pow__(self, other):
+        return self.salary**other.salary
+
+
+e1 = employee()
+e2 = employee()
+e1.salary = 100
+e2.salary = 200
+print(e1**e2)
+
+# create a class c-2d vector and use it to create another class c-3d vector
+
+
+class C2dVec:
+    def __init__(self, i, j):
+        self.icap = i
+        self.jcap = j
+
+    def __str__(self):
+        return f"{self.icap}i + {self.jcap}j"
+
+
+class C3dVec(C2dVec):
+    def __init__(self, i, j, k):
+        super().__init__(i, j)
+        self.kcap = k
+
+    def __str__(self):
+        return f"{self.icap}i + {self.jcap}j + {self.kcap}k"
+
+
+v2d = C2dVec(1, 3)
+v3d = C3dVec(1, 9, 7)
+print(v2d)
+print(v3d)
+
+#  for more details go to python official documentation of data model
+
+# create a class pet from a class animals and further create a class dog from pet class add the method bark to the dog class
+
+
+class animal:
+    animalType = "Mammal"
+
+
+class pet:
+    color = "white"
+
+
+class dog:
+    @staticmethod
+    def bark():
+        print("bow bow !!")
+
+
+d = dog()
+d.bark()
+
+# multilevel inheritance
+
+# create a class employee and add salary and increment properties to it. write a method salaryAfterIncrement method with a @property decorator with a setter which changes the value of increment based on the salary value as given below.
+
+
+class employee:
+    salary = 1000
+    increment = 1.5
+
+    @property
+    def salaryAfterIncrement(self):
+        return self.salary * self.increment
+
+    @salaryAfterIncrement.setter
+    def salaryAfterIncrement(self, sai):
+        self.increment = sai / self.salary
+
+
+e = employee()
+print(e.salaryAfterIncrement)
+e.salaryAfterIncrement = 2000
+print(e.increment)
+
+# decorators : A decorator is a design pattern in Python that allows a user to add new functionality to an existing object without modifying its structure. Decorators are usually called before the definition of a function you want to decorate. In this tutorial, you'll learn how you can create decorators and how to use them. Decorators are very powerful and useful tool in Python since it allows programmers to modify the behavior of function or class. Decorators allow us to wrap another function in order to extend the behavior of the wrapped function, without permanently modifying it. In Decorators, functions are taken as the argument into another function and then called inside the wrapper function.
+
+#  write a class complex to represent complex numbers, along overloaded operators +, -, and * which adds, subtracts, and multiplies complex numbers respectively.
+
+
+# Define a class Complex to represent complex numbers
+class Complex:
+    def __init__(self, r, i):
+        self.real = r
+        self.imaginary = i
+        
+    # Overload the addition operator (+) to add two complex numbers
+    def __add__(self,c):
+        return Complex(self.real + c.real , self.imaginary + c.imaginary)
+    
+    # Overload the multiplication operator (*) to multiply two complex numbers
+    def __mul__(self,c):
+        return Complex(self.real*c.real - self.imaginary*c.imaginary, self.real*c.imaginary + self.imaginary*c.real)
+    
+    # Override the string representation of the complex number
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+
+# Create two complex numbers
+c1 = Complex(1, 4)
+c2 = Complex(3, 4)
+
+# Perform addition and multiplication of complex numbers
+print(c1+c2)
+print(c1*c2)
+
+
+# method overriding : Method overriding is a feature that allows a subclass to provide a specific implementation of a method that is already provided by its parent class. When a method in a subclass has the same name, same parameters or signature and same return type(or sub-type) as a method in its super-class, then the method in the subclass is said to override the method in the super-class.
--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,20 @@
-
-class employee:
-    company = "google"
-    salary = 100
-    location= "delhi"
+class Complex:
+    def __init__(self, r, i):
+        self.real = r
+        self.imaginary = i
+        
+    def __add__(self,c):
+        return Complex(self.real + c.real , self.imaginary + c.imaginary)
     
-e = employee()
-print(e.salary)
+    def __mul__(self,c):
+        return Complex(self.real*c.real - self.imaginary*c.imaginary, self.real*c.imaginary + self.imaginary*c.real)
+    
+    def __str__(self):
+        return f"{self.real} + {self.imaginary}i"
+
+    
+
+c1 = Complex(1, 4)
+c2 = Complex(3, 4)
+print(c1+c2)
+print(c1*c2)
